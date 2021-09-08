3e47c215172977893f7976e6433a7f5404260731
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-09-08 15:03:16 +0200

    New img with Automatic deploy

--- client/src/Component/Gallery.js
+++ client/src/Component/Gallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let urlm = {
-  imageUrl1:"https://lh3.googleusercontent.com/d/1ZsRHQ_cl05QEypsodoCXQDADVndco0Cb=w686-h228-n?authuser=0"
+  imageUrl1:"http://prfrankild.dk/ArtWalk_Logo%20final_ww.png"
 }
 
 function Gallery(props) {
@@ -10,7 +10,7 @@ function Gallery(props) {
      
 
 <div className="img-with-text">
-      <img alt="" src={urlm.imageUrl1} />
+      <img alt="" height="270" src={urlm.imageUrl1} />
         </div>
   </div>
 );
